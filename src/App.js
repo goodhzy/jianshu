24da00f9acea8fadcb705d8a51637194ae34d8c7
goodhzy <qq.com>
2020-06-11 22:14:46 +0800

    使用immutable管理state

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
     <Provider store={store}>
       <Header />
     </Provider>
-    
   );
 }
 
--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,17 +1,17 @@
 import * as actionTypes from './actionTypes'
+import { fromJS } from 'immutable'
 
-const defaultStore = {}
+const defaultStore = fromJS({
+    focused: false
+})
 
 export default (state=defaultStore, action)=>{
     if(action.type === actionTypes.SEARCH_FOCUS){
-        return {
-            focused: true
-        }
+        return state.set('focused', true)
     }
     if(action.type === actionTypes.SEARCH_BLUR){
-        return {
-            focused: false
-        }
+        return state.set('focused', false)
+
     }
     return state;
 }
