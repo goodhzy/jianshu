c2620f6f2559fcc7280798cb01649e2f651e7ac3
goodhzy <qq.com>
2020-06-13 00:11:34 +0800

    完成首页功能的开发

--- src/pages/home/store/index.js
+++ src/pages/home/store/index.js
@@ -1,9 +1,7 @@
-import reducer from './reducer'
-import * as actionTypes from './actionTypes'
+import reducer from "./reducer";
 import * as actionCreators from './actionCreators'
 
 export {
     reducer,
-    actionTypes,
     actionCreators
 }
